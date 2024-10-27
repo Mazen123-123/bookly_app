308b51041bb8e6d6051e33514c2d827c96733115
Mazen123-123 <mazentayal2@gmail.com>
2024-10-27 17:54:01 +0300

    Best seller last view item part

--- lib/Freatures/home/presentation/views/widgets/home_view_body.dart
+++ lib/Freatures/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'custom_app_bar.dart';
@@ -21,6 +22,40 @@ class HomeViewBody extends StatelessWidget {
           Text(
             "Best Seller",
             style: Styles.titleMedium,
+          ),
+          BestSellerListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 130,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.amberAccent,
+                image: const DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(
+                    AssetsData.testImage,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Column(
+            children: [],
           )
         ],
       ),
